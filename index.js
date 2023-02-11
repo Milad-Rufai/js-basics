ab1bdf296f9cd452d5b02bca0913d4034e9c3e52
Milad-Rufai <temilader19@gmail.com>
2023-02-11 11:01:17 +0100

    JavaScript tasks

---
+++ index.js
@@ -0,0 +1,20 @@
+// Task - Write a function that multiples two numbers. Call that function to multiply any two numbers of your choice and log the answer in your console.
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiply(4, 5));
+
+
+// Task 2 - Create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. Use these variables to form a sentence and log the sentence in the console.
+
+let first_name = 'Temilade';
+let last_name = 'Rufai';
+let Stutern_track = 'Frontend Development Track';
+let skills = 'Graphics Design and Website Development';
+let favourite_color = 'Blue'
+let nigerian_independence = 1960
+
+console.log('My name is ' + first_name, last_name + ', My favourite color is ' + favourite_color + '. I am currently enrolled in the Stutern ' + Stutern_track + ', my skills includes; ' + skills + '.')
+console.log('On the 1st of October ' + nigerian_independence + ', Nigeria gained independence from the British colonial rule.')
